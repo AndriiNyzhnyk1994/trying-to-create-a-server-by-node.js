42896ac12eafa3a42b18b8147b7ff8e08c6a5f77
Andrii Nyzhnyk <prostoandrew1994@gmail.com>
2023-09-07 14:45:11 +0300

    added switch/case logic to calling url parameters

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const http = require('http')
 
-const server = http.createServer(
-    (request, response) => { 
-        response.write('Hey')
-        response.end()
+
+let requestsCount = 0
+
+const server = http.createServer((request, response) => {
+    requestsCount++
+
+    switch (request.url) {
+        case '/students':
+            response.write('students ')
+            break;
+        case '/courses':
+            response.write('React, TS, Redux')
+            break;
+        default: response.write('404 Not found')
+
     }
+
+    response.write(' Hey ' + requestsCount)
+    response.end()
+}
 )
 
 server.listen(3003)
